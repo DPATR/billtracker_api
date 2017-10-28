82cc55f78d5ba045c70ecda68cca2444ad81a93f
DPATR <denise.patriquin@libertymutual.com>
2017-10-27 20:31:50 -0400

    Commit to modify Serializer to remove user object from being returned to Client

--- app/serializers/bill_serializer.rb
+++ app/serializers/bill_serializer.rb
@@ -1,3 +1,4 @@
+# modified 10-27-17 to remove :user_id and :user so they are not passed back to the client
 class BillSerializer < ActiveModel::Serializer
-  attributes :id, :creditor, :billing_month, :amount_due, :user_id, :user
+  attributes :id, :creditor, :billing_month, :amount_due
 end
